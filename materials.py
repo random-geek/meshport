c816a964b7319f09f6ddd0f105f3517597c9068c
GreenDimond <24834740+GreenXenith@users.noreply.github.com>
2019-07-01 12:03:41 -0700

    Include transparency and material rewrite in script

---
+++ materials.py
@@ -0,0 +1,65 @@
+# Original script (interpolation): Techy5
+# Expanded script: GreenXenith
+
+### Use better materials and support alpha ###
+# Usage: Open or copy script in Blender
+# Run script while object is selected
+
+import bpy
+
+for mat in bpy.data.materials:
+    try:
+        nodes = mat.node_tree.nodes
+        links = mat.node_tree.links
+
+        # Remove all nodes except texture
+        for node in nodes:
+            if node.type != "TEX_IMAGE":
+                nodes.remove(node)
+
+        # Change image interpolation
+        tex = nodes["Image Texture"]
+        tex.interpolation = "Closest"
+        tex.location = 0, 0
+        
+        # Create texture coordinate node
+        coord = nodes.new("ShaderNodeTexCoord")
+        coord.location = -600, 0
+
+        # Create mapping node
+        map = nodes.new("ShaderNodeMapping")
+        map.location = -400, 0
+        
+        # Create diffuse shader
+        diff = nodes.new("ShaderNodeBsdfDiffuse")
+        diff.location = 200, 0
+        
+        # Create transparent shader
+        trans = nodes.new("ShaderNodeBsdfTransparent")
+        trans.location = 200, -150
+
+        # Create mix shader
+        mix = nodes.new("ShaderNodeMixShader")
+        mix.location = 400, 0
+
+        # Create output
+        out = nodes.new("ShaderNodeOutputMaterial")
+        out.location = 600, 0
+        
+        # Link everything
+        links.new(coord.outputs[2], map.inputs[0]) # Coord > Map
+        links.new(map.outputs[0], tex.inputs[0]) # Map > Texture
+        links.new(tex.outputs[0], diff.inputs[0]) # Texture > Diffuse
+        links.new(diff.outputs[0], mix.inputs[2]) # Diffuse > Mix
+        
+        links.new(trans.outputs[0], mix.inputs[1]) # Transparent > Mix
+        links.new(tex.outputs[1], mix.inputs[0]) # Texture alpha > Mix factor
+
+        links.new(mix.outputs[0], out.inputs[0]) # Mix > Output
+        
+        # Deselect all
+        for node in nodes:
+            node.select = False
+                
+    except:
+        continue
